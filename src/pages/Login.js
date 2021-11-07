9c38ebb67748ac6a101af041d21be7b213f11288
alex <axl3210@rit.edu>
2021-11-07 01:57:58 -0400

    update to class name

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,13 +5,13 @@ class Login extends React.Component {
         return (
             <div id="signIn">
                 <form id="signinForm">
-                    <label class="signinFormlabel" htmlFor="uname"><b>Username</b></label>
-                    <input class="signinFormInput" type="text" placeholder="Username" name="uname" required />
+                    <label className="signinFormlabel" htmlFor="uname"><b>Username</b></label>
+                    <input className="signinFormInput" type="text" placeholder="Username" name="uname" required />
 
                     <br />
 
-                    <label class="signinFormlabel" htmlFor="psw"><b>Password</b></label>
-                    <input class="signinFormInput" type="password" placeholder="Password" name="psw" required />
+                    <label className="signinFormlabel" htmlFor="psw"><b>Password</b></label>
+                    <input className="signinFormInput" type="password" placeholder="Password" name="psw" required />
                     <br />
 
                     <button id="signinFormSubmit" type="submit">Login</button>
